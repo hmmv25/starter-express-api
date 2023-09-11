0a40a7de7fd898b2bec55383fece6749bd228ae6
hmmv25 <144031961+hmmv25@users.noreply.github.com>
2023-09-11 14:39:18 +0200

    Update xml2js.js

--- routes/xml2js.js
+++ routes/xml2js.js
@@ -5,9 +5,14 @@ router.get('/xml2js/', function(req, res, next) {
   res.render('xml2js', {});
 });
 
-// xml2js and express-xml-bodyparser example: retrieve customer using XML
 router.post('/sap/bc/srt/scs_ext/sap/fixedassetcreatemain', (req, res, next) => {
-  console.log('Raw XML: ' + req.rawBody);
+  console.log('Raw Asset XML: ' + req.rawBody);
+  //console.log('Parsed XML: ' + JSON.stringify(req.body));
+  res.status(200).send(req.rawBody);
+});
+
+router.post('/sap/bc/srt/scs/sap/businesspartnersuitebulkreplic', (req, res, next) => {
+  console.log('Raw BP XML: ' + req.rawBody);
   //console.log('Parsed XML: ' + JSON.stringify(req.body));
   res.status(200).send(req.rawBody);
 });
