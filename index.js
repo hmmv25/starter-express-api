109045db06e58b871769e00f983d29fc8dd5c36f
hmmv25 <144031961+hmmv25@users.noreply.github.com>
2023-09-11 14:28:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,15 @@ app.set('port', port);
 
 var server = http.createServer(app);
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(xmlparser());
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo 123456')
+    res.send('Yo 1234567')
 })
 
 //app.listen(process.env.PORT || 3000)
