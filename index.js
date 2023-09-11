975691d2a443642bbcd9ca41e6789dd72962d9df
hmmv25 <144031961+hmmv25@users.noreply.github.com>
2023-09-11 14:16:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+
+var port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+var server = http.createServer(app);
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+
+//app.listen(process.env.PORT || 3000)
+server.listen(port);
