af46a7a472bb3a72a26ee81c420e0dbaa842735c
hmmv25 <144031961+hmmv25@users.noreply.github.com>
2023-09-11 14:17:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,22 @@ app.all('/', (req, res) => {
 })
 
 //app.listen(process.env.PORT || 3000)
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
 server.listen(port);
