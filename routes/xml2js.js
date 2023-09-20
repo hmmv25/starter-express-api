3424200be3cc18cbce5a07c805ea8b95d18c372f
hmmv25 <144031961+hmmv25@users.noreply.github.com>
2023-09-20 13:21:52 +0200

    Update xml2js.js

--- routes/xml2js.js
+++ routes/xml2js.js
@@ -22,6 +22,7 @@ router.post('/CO_TAX_JURI_DETER_SPRX', (req, res, next) => {
   console.log('External Tax - Determine Tax Jurisdiction Codes: ' + req.rawBody);
   //console.log('Parsed XML: ' + JSON.stringify(req.body));
   //res.status(200).send(req.rawBody);
+  res.set('content-type', 'application/soap+xml; charset=utf-8');
   res.status(200).send('<soap-env:Envelope xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/"><soap-env:Header/><soap-env:Body><n0:TAX_JURISDICTION_RECEIVE xmlns:n0="http://sap.com/xi/FotETaxUS" xmlns:prx="urn:sap.com:proxy:X0E:/1SAI/TAS504F73D3B7261E3994BE:793"><LOCATION_HEADER><RETCODE>0</RETCODE><ERRCODE/><ERRMSG/></LOCATION_HEADER><LOCATION_RESULTS><COUNTRY>US</COUNTRY><STATE>GA</STATE><COUNTY/><CITY>Atlanta</CITY><ZIPCODE>30315-1402</ZIPCODE><TXJCD_L1>00</TXJCD_L1><TXJCD_L2>00</TXJCD_L2><TXJCD_L3>00</TXJCD_L3><TXJCD_L4>00</TXJCD_L4><TXJCD>012345678901234</TXJCD><OUTOF_CITY/><REF_INFO/></LOCATION_RESULTS></n0:TAX_JURISDICTION_RECEIVE></soap-env:Body></soap-env:Envelope>');
 });
 
